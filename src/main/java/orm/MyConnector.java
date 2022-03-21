cb4c24cf2cb7d14eab5578d1401dd50c2b41ba15
Galin Slavov <galindiqnow@gmail.com>
2022-03-21 15:05:40 +0200

    add into entityManager

--- src/main/java/orm/MyConnector.java
+++ src/main/java/orm/MyConnector.java
@@ -7,17 +7,19 @@ import java.util.Properties;
 
 public class MyConnector {
     private static Connection connection;
-    private  static  final String jdbcSting = "jdbc:mysql://localhost:3306/";
+    private static final String jdbcString = "jdbc:mysql://localhost:3307/";
 
-    private static void createConnection(String user, String password, String dbName) throws SQLException {
+    private MyConnector() {}
+
+    public static void createConnection(String user, String password, String dbName) throws SQLException {
         Properties properties = new Properties();
         properties.setProperty("user", user);
-        properties.setProperty("password",password);
+        properties.setProperty("password", password);
 
-        connection = DriverManager.getConnection(jdbcSting + dbName,properties);
+        connection = DriverManager.getConnection(jdbcString + dbName, properties);
     }
 
-    public static Connection getConnection(){
-        return  connection;
+    public static Connection getConnection() {
+        return connection;
     }
 }
