2fe1b5f66f8eb6e18966a32084f988ad7b5aa0bf
Galin Slavov <galindiqnow@gmail.com>
2022-03-21 15:11:19 +0200

    add User

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -1,20 +1,45 @@
 package entities;
 
+import annotations.Column;
+import annotations.Entity;
+import annotations.Id;
+
 import java.time.LocalDate;
 
+@Entity(name = "users")
 public class User {
+
+    @Id
+    @Column(name = "id")
     private long id;
 
+    @Column(name = "username")
     private String username;
 
+    @Column(name = "age")
     private int age;
 
-    private LocalDate registration;
+    @Column(name = "registration_date")
+    private LocalDate registrationDate;
 
-    public User(String username, int age, LocalDate registration){
+    @Column(name = "last_logged_in")
+    private LocalDate lastLoggedIn;
+
+    public User() {}
+
+    public User(String username, int age, LocalDate registrationDate) {
         this.username = username;
         this.age = age;
-        this.registration = registration;
+        this.registrationDate = registrationDate;
+        this.lastLoggedIn = LocalDate.now();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getUsername() {
@@ -33,11 +58,30 @@ public class User {
         this.age = age;
     }
 
-    public LocalDate getRegistration() {
-        return registration;
+    public LocalDate getRegistrationDate() {
+        return registrationDate;
     }
 
-    public void setRegistration(LocalDate registration) {
-        this.registration = registration;
+    public void setRegistrationDate(LocalDate registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public LocalDate getLastLoggedIn() {
+        return lastLoggedIn;
+    }
+
+    public void setLastLoggedIn(LocalDate lastLoggedIn) {
+        this.lastLoggedIn = lastLoggedIn;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", age=" + age +
+                ", registrationDate=" + registrationDate +
+                ", lastLoggedIn=" + lastLoggedIn +
+                '}';
     }
 }
